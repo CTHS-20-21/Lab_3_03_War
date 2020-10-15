0b615d8466d0be6e86bd3513416a5a7ab35d4e47
Fennell, Scott P 263712616 <scott.p.fennell@nasa.gov>
2020-10-15 11:24:30 -0500

    add starter contracts

--- war.py
+++ war.py
@@ -16,4 +16,20 @@ print("The starter deck looks like this:\n" + str(deck)) # delete this line of c
 
 #################################
 
-# Your code here
+# Your code here (write the functions below, and complete the rest of the game)
+
+
+# Write the function based on the contract
+# name: player_turn
+# purpose: takes in a player name and draws/removes a card from the deck, prints "user drew card x", and returns the value
+# Arguments: player_name as string, deck as list
+# returns: integer
+
+
+# Finish the contract and write the function based on the contract
+# name: compare_scores (Write your own contract!)
+# purpose:
+# Arguments:
+# returns:
+
+
